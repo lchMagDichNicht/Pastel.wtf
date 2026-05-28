5ee9c1f7ca59ec561598c7ef08629f6ca3aa082d
lchMagDichNicht <maxi73118@gmail.com>
2026-05-29 00:03:22 +0200

    :3

---
+++ Main/Aimbot.lua
@@ -0,0 +1,31 @@
+return function(Tabs, Library)
+
+    -- =========================
+    -- Service
+    -- =========================
+    local Players = game:GetService("Players")
+    local RunService = game:GetService("RunService")
+    local LocalPlayer = Players.LocalPlayer
+    local cam = workspace.CurrentCamera
+    local UserInputService = game:GetService("UserInputService")
+    local UIS = game:GetService("UserInputService")
+    local Lighting = game:GetService("Lighting")
+    local Terrain = workspace:FindFirstChildOfClass("Terrain")
+    local Camera = workspace.CurrentCamera
+    local Options = Library.Options
+    local Toggles = Library.Toggles
+    local player = Players.LocalPlayer
+    local Player = Players.LocalPlayer
+    local CoreGui = game:GetService("CoreGui")
+    local VirtualInputManager = game:GetService("VirtualInputManager")
+    local Gui = Player:WaitForChild("PlayerGui")
+    local TweenService = game:GetService("TweenService")
+    local Stats = game:GetService("Stats")
+    local HttpService = game:GetService("HttpService")
+
+
+
+
+
+
+end
