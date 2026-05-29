85dc9dd72a7f42177eddbfd57cbba9076cd1cfed
lchMagDichNicht <maxi73118@gmail.com>
2026-05-30 01:10:11 +0200

    :3

--- Loader.lua
+++ Loader.lua
@@ -1,2 +1,1 @@
-getgenv().SCRIPT_KEY = "KEYLESS"
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/69a7532d59f5e62a59d4a4bda06f24243f5c6260e04ac85bb5ac2587e6fab33a/download"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/0f2557e9d2b9009906e641bdba7ec9b7.lua"))()
